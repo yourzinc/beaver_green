855cac11d705cc34b3772de6f1e9559e25da8cbb
woozoo_zinc <your_zinc@ewhain.net>
2021-11-14 17:46:18 +0900

    main.html & 1.css

---
+++ function.js
@@ -0,0 +1,9 @@
+const btn2_bg =document.querySelector(".b2-bg1-1");
+const btn2 = document.querySelector(".button2");
+
+btn2.addEventListener("click", function()
+{  
+    console.log = "click";
+    btn2_bg.style.color = null;
+}
+)
