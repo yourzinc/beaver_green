8fa1dd9e1a5b127f231520a8cf8a0c85a9ec90db
woozoo_zinc <your_zinc@ewhain.net>
2021-11-22 04:48:36 +0900

    main.ejs & video.html

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const app = express();
+
+let isPlayingNumber = 1;
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+    res.render('main', {choice:0});
+  });
+
+app.post("/post", function(req, res){
+  if(req.body.number == isPlayingNumber)
+  {
+    res.json({isPlaying : true});
+    console.log(true);
+  }
+  else {
+    res.json({isPlaying : false});
+  }
+
+
+})
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
+
+/*
+
 var http = require('http');
 var fs = require('fs');
+
 var app = http.createServer(function(request,response){
     var url = request.url;
     if(request.url == '/'){
@@ -7,7 +45,8 @@ var app = http.createServer(function(request,response){
     }
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
- 
 });
+
 app.listen(3000);
 
+*/
